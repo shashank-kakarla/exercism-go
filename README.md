e031e08f14991d207980c252fa047f65974e243c
shashank-kakarla <nsskakarla@gmail.com>
2021-06-13 22:53:17 -0400

    Updated the Scrabble code

---
+++ README.md
@@ -0,0 +1,19 @@
+# Go Track
+
+This is the Golang track from [Exercism](https://exercism.io/go)
+
+## Running
+
+You can run the files by the command ```go run <FILENAME>.go``` when in the respective directory.
+
+## Running Tests and Benchmarks
+### Tests
+To run the tests use the command
+```
+go test
+```
+### Benchmarks
+To run the benchmarks use the command
+```
+go test -v --bench . --benchmem
+```
--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,24 +1,23 @@
 // Package scrabble has a function that calculates the score
 package scrabble
 
-import (
-	"strings"
-)
+import "unicode"
+
+var alphabetScore = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10}
 
 // Score returns the score of the word.
 func Score(word string) int {
-	alphabetScore := map[string]int{
-		"A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
-		"D": 2, "G": 2,
-		"B": 3, "C": 3, "M": 3, "P": 3,
-		"F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
-		"K": 5,
-		"J": 8, "X": 8,
-		"Q": 10, "Z": 10}
+
 	sum := 0
-	runeWord := []rune(strings.ToUpper(word))
-	for i := range runeWord {
-		sum += alphabetScore[string(runeWord[i])]
+	for _, r := range word {
+		sum += alphabetScore[unicode.ToUpper(r)]
 	}
 	return sum
 }
